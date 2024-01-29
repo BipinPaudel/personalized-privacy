854a673455c825d4f8270b0732a5ff8665437db1
Bipin Paudel <paudelbipin.bp@gmail.com>
2024-01-29 07:30:52 -0600

    Basic experiment done

--- main.py
+++ main.py
@@ -8,53 +8,66 @@ from user import User
 from server import Server
 import matplotlib.pyplot as plt
 import random
-
+import sys
 if __name__=='__main__':
     
-    number_of_users = [100, 1000, 10000, 100000]
-    number_of_users = [100, 1000, 10000]
+    number_of_users = [10000]
     for n in number_of_users:
         accuracy = 0
-         
-        plt.clf()
-        # evolving_histogram_exp = []
-        for exp_ins in range(1):
+        
+        for exp_ins in range(2):
             data = gen_binary_data(n=n, one_prob=0.3)
             k = 2
+            plt.clf()
             real_freq = np.unique(data, return_counts=True)[-1] / n
-            num_seed = 50
+            num_seed = 20
 
             
             print(f'Real frequency {real_freq}')
-            epsilons = [[2,0.1], [0.1, 0.6], [0.1,0.8],[0.1,1], [0.1,1], [0.1,1],[0.1,1],[0.1,1],[0.1,1.2],[0.1,1.4],[0.1,1.4],[0.1,1.4]]
+            # epsilons = [[5,0.5], [0.7, 1], [0.8,1.5],[1, 2]] + [[1.5,2.4] for _ in range(1)]
+
+            # epsilons = [[2.6,0.4],[minority_epsilon,0.8],[minority_epsilon,1],[minority_epsilon,1.5],
+            #              [minority_epsilon,1.8]] + [[minority_epsilon,2] for _ in range(25)]
+            eps_inf = 2.6
+            # e1 = 0.38
+            # epsilons = np.array([[2.6, e1], [0.8, e1], [0.8, e1], [0.8, e1]])
+            
+            #### temp
+            e1 = 0.5067
+            epsilons = np.array([[2.6, e1]] + [[0.8, e1]]*3)
+
+
             
 
-            epsilons = [[2.6,0.01],[0.01,0.8],[0.01,1],[0.01,1.5],[0.01,2], [0.01,2.5],[0.01,2.5],[0.01, 2.5], [0.01,2.5]] + 20 * [[0.01,2.5]]
+            if exp_ins == 1: 
+                epsilons = [[2.6,1.52]]
+            # epsilons = [[2.6,0.4],[0.4,0.8],[0.4,1],[0.4,1.5], [0.4,1.8], [0.4,2], [0.4,2], [0.4,2], [0.4,2]]
+
+            # epsilons = [[2.6,0.01],[0.01,0.8],[0.01,1],[0.01,1.5],[0.01,2], [0.01,2.5],[0.01,2.5],[0.01, 2.5], [0.01,2.5]] + 20 * [[0.01,2.5]]
             # epsilons = [[2.6,0.5],[0.5,0.8],[0.5,1],[0.5,1.5]]
 
             times = len(epsilons)
             
             estimated_histogram = None
             average_histograms = []
-            evolving_histogram_seed = []    
             dic_mse = {seed: {
                 } for seed in range(num_seed)
             }
-
+            # np.random.seed(100)
             for seed in range(num_seed):
 
                 
-            #permanent randomization phase
+                #permanent randomization phase
                 server = Server(epsilons, k)
                 eps, p1, q1 = server.get_permanent_randomization_configs()
                 
                 evolving_histogram = []
                 #instantiate all users with their private value
                 users = [User(v) for v in data['v']]
-
+                total_maj_loss = total_min_loss = 0
                 #permanent randomization with high epsilon in the beginning
                 permanent_values = [user.permanent_randomization(p1, q1) for user in users]
-
+                
                 #number of interactions between user and server
                 for time in range(times):
                     #everyone uses same epsilon or privacy mechanism for the first time
@@ -64,163 +77,58 @@ if __name__=='__main__':
                         for u_ind, user in enumerate(users):
                             report = user.instantaneous_randomization(p1, q1, p2, q2)
                             reports.append(report)
-                        estimated_histogram = server.get_estimated_histogram(reports, time)
-                        print(time)
+                        estimated_histogram = server.get_estimated_histogram(reports, time, data['v'])
+                        if estimated_histogram[0] < estimated_histogram[1]:
+                            print('flipped')
+                            sys.exit()
                         dic_mse[seed][0] = mean_squared_error(real_freq, estimated_histogram)
-
+                        
                     else:
-
                         #at time > 0, there will be a latest histogram that gives information to user about maj and min
                         eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj = server.get_instantaneous_randomization_configs(time)
-                        majority = estimated_histogram.argmax()
-                        minority = estimated_histogram.argmin()
+                        majority = 0 #estimated_histogram.argmax()
+                        minority = 1 #estimated_histogram.argmin()
                         reports = []
                         for u in users:
                             # if user's private value belongs to majority they'll apply p_maj and q_maj corresponding to higher epsilon
+                            
+                            # if u.private_value == majority:
+                            #     report = u.instantaneous_randomization(p1, q1, p_maj, q_maj)
+                            # else:
+                            #     report = u.instantaneous_randomization(p1, q1, p_min, q_min)
+
                             if u.private_value == majority:
-                                report = u.instantaneous_randomization(p1, q1, p_maj, q_maj)
+                                if np.random.binomial(1, estimated_histogram[0]):
+                                    report = u.instantaneous_randomization(p1, q1, p_maj, q_maj)
+                                else:
+                                    report = u.instantaneous_randomization(p1, q1, p_min, q_min)
                             else:
-                                report = u.instantaneous_randomization(p1, q1, p_min, q_min)
+                                if np.random.binomial(1, estimated_histogram[1]):
+                                    report = u.instantaneous_randomization(p1, q1, p_maj, q_maj)
+                                else:
+                                    report = u.instantaneous_randomization(p1, q1, p_min, q_min)
                             reports.append(report)
-                        estimated_histogram = server.get_estimated_histogram(reports, time, private_list = np.array(data['v']))
-                        print(f'Seed: {seed}, users: {n}, time: {time}, estimated: {estimated_histogram}')
+                        estimated_histogram = server.get_estimated_histogram(reports, time, data['v'])
+                        if estimated_histogram[0] < estimated_histogram[1]:
+                            print('flipped')
+                            sys.exit()
                         dic_mse[seed][time] = mean_squared_error(real_freq, estimated_histogram)
+                    print(f'Seed: {seed}, Real freq: {real_freq}, users: {n}, time: {time}, estimated: {estimated_histogram}')
 
-                    evolving_histogram.append(estimated_histogram)
-                evolving_histogram_seed.append(evolving_histogram)
-            evolving_histogram_seed = np.array(evolving_histogram_seed)
 
-            # evolving_histogram_exp.append(evolving_histogram)
-        # evolving_histogram_exp = np.array(evolving_histogram_exp)
 
 
             errors = []
             first_error = None
             for time in range(times):
                 errors.append(np.mean([dic_mse[seed][time] for seed in range(num_seed)]))
-            print(errors)
-            x_axis = [i+1 for i in range(times)]
+            for er in errors:
+                print(f'{er:.8f}')
+            x_axis = [int(i+1) for i in range(times)]
             plt.plot(x_axis, errors)
+            first_txt = f'First error: {errors[0]:.8f}'
+            last_txt = f'Last error: {errors[-1]:.8f}'
+            plt.text(1,errors[0],first_txt+"\n"+last_txt)
             plt.savefig(f'errors_nusers_{n}_exp_instance_{exp_ins}_accuracy_{accuracy}.png')
 
         print(accuracy)
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # for seed in range(num_seed):
-    #     for ep in eps:
-    #         grr_reports = [GRR_Client(input_data, k, ep) for input_data in data['v']]
-    #         grr_est_freq = GRR_Aggregator_IBU(grr_reports, k, ep)
-    #         print(f'Estimated frequency at seed: {seed}, epsilon: {ep}, ==> {grr_est_freq}')
-    #         dic_mse[seed]["GRR"].append(mean_squared_error(real_freq, grr_est_freq))
-
-    # mean_errors = np.mean([dic_mse[seed]["GRR"] for seed in range(num_seed)], axis=0)
-
-    # print(f'Mean grr errors: {mean_errors}')
-
-    # dic_mse = {seed: {
-    #     "LGRR": [],
-    # } for seed in range(num_seed)}
-
-    # for seed in range(num_seed):
-    #     for ep in eps:
-    #         l_grr_reports = [L_GRR_Client(input_data, k, ep, 0.7 * ep) for input_data in data['v']]
-    #         grr_est_freq = L_GRR_Aggregator_MI(l_grr_reports, k, 1.3 * ep, ep)
-    #         print(f'Estimated frequency at seed: {seed}, epsilon: {ep}, ==> {grr_est_freq}')
-    #         dic_mse[seed]["LGRR"].append(mean_squared_error(real_freq, grr_est_freq))
-
-    # mean_errors = np.mean([dic_mse[seed]["LGRR"] for seed in range(num_seed)], axis=0)
-
-    # print(f'Mean lgrr errors: {mean_errors}')
-
-
-
-    '''
-    REAL interaction between server and users
-    '''
-    # number_of_interactions = 3
-    # num_seed = 1
-    # dic_mse = {seed: {
-    #     "LGRR": [],
-    # } for seed in range(num_seed)}
-
-
-    # epsilons_majority = [0.2, 0.5, 0.7]
-    # epsilons_minority = [0.2, 0.25, 0.3]
-
-    # for interaction in range(number_of_interactions):
-    #     for seed in range(num_seed):
-    #         if interaction == 0:
-    #             eps_inf = 2.0
-    #             l_grr_reports = np.array([L_GRR_Client(input_data, k, eps_inf, epsilons_majority[interaction]) for input_data in data['v']])
-    #             permanent_reports = l_grr_reports[:,0]
-    #             randomized_reports = l_grr_reports[:,1]
-    #             ps = l_grr_reports[:, 2, 4]
-    #             qs = l_grr_reports[:, 3, 5]
-    #             grr_est_freq = L_GRR_Aggregator_MI(randomized_reports, k, eps_inf, epsilons_majority[interaction])
-    #             print(f'Estimated frequency, epsilon perm: {eps_inf}, eps1: {epsilons_majority[interaction]} ==> {grr_est_freq}')
-    #             dic_mse[seed]["LGRR"].append(mean_squared_error(real_freq, grr_est_freq))
-    #             mean_errors = np.mean([dic_mse[seed]["LGRR"] for seed in range(num_seed)], axis=0)
-    #             print(f'Mean lgrr errors: {mean_errors}')
-    #             randomized_reports = randomized_reports.reshape(-1,1)
-    #             # ps = ps.reshape(-1, 1)
-    #             # qs = qs.reshape(-1, 1)
-            
-    #         else:
-
-    #             rand_op = []
-    #             p_list = []
-    #             q_list = []
-    #             majority = grr_est_freq.argmax()
-    #             minority = grr_est_freq.argmin()
-    #             for input in permanent_reports:
-                    
-    #                 if input == majority:
-    #                     op, p, q  = L_GRR_Client_Interaction(input, k, eps_inf, epsilons_majority[interaction-1])
-    #                 else:
-    #                     op, p, q = L_GRR_Client_Interaction(input, k, eps_inf, epsilons_minority[interaction-1])
-    #                 rand_op.append(op)
-    #                 p_list.append(p)
-    #                 q_list.append(q)
-                
-    #             randomized_reports = np.concatenate((randomized_reports, rand_op.reshape(-1,1)), axis=1)
-    #             ps = np.concatenate((ps, p.reshape(-1, 1)), axis=1)
-    #             qs = np.concatenate((qs, q.reshape(-1, 1)), axis=1)
-
-    #             grr_est_freq = GRR_Aggregator_IBU_interact(randomized_reports, k, grr_est_freq, epsilons_minority, epsilons_majority, interaction, ps, qs)
-
-
-                    
-
-
-
-                
-
-
-
-
-
-
--- server.py
+++ server.py
@@ -9,7 +9,15 @@ class Server:
         self.t = len(self.epsilons)
         self.noisy_reports = None
         self.histograms = None
+        self.p_b_bstar = None
+        self.hist = np.zeros(2)
+        eps_perm, p1, q1 = self.get_p1_q1()
+        self.p_bstar_b = np.array([
+            [p1, q1], 
+            [q1, p1]
+        ])
 
+        
     def get_p1_q1(self):
         eps_perm = self.epsilons[0][0]
         p1 = np.exp(eps_perm) / (np.exp(eps_perm) + self.k - 1)
@@ -26,8 +34,10 @@ class Server:
         # GRR parameters for round 2
             eps_1 = self.epsilons[t][1]
             #calculating probability for instantaneous randomization from epsilon_1 for time = 0
-            p2 = (q1 - np.exp(eps_1) * p1) / ((-p1 * np.exp(eps_1)) + self.k*q1*np.exp(eps_1) - q1*np.exp(eps_1) - p1*(self.k-1)+q1)
-            q2 = (1 - p2) / (self.k-1)
+            # p2 = (q1 - np.exp(eps_1) * p1) / ((-p1 * np.exp(eps_1)) + self.k*q1*np.exp(eps_1) - q1*np.exp(eps_1) - p1*(self.k-1)+q1)
+            # q2 = (1 - p2) / (self.k-1)
+            p2 = np.exp(eps_1) / (np.exp(eps_1) + self.k - 1)
+            q2 = (1 - p2) / (self.k - 1)
             return eps_perm, p1, q1, eps_1, p2, q2
         
         #calculating probability for instantaneous randomization from epsilon_maj and epsilon_min for time > 0
@@ -41,205 +51,131 @@ class Server:
 
         return eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj
 
-    def estimate_first_time(self, nr):
-        eps_perm = self.epsilons[0][0]
-        eps_1 = self.epsilons[0][1]
-
-        return L_GRR_Aggregator_MI(nr, self.k, eps_perm, eps_1)
-
     '''
     size(self.noisy_reports_probability) = (times,n,2), at each time, for each user, calculates the prob that 
     user belongs to majority and minority
-
-
     '''
     def get_estimated_histogram(self, nr, time, private_list=None):
         if time == 0:
-            estimated_histogram = self.perform_first_time_estimation(nr, time)
-            return estimated_histogram
+            estimated_histogram = self.perform_first_time_estimation(nr, time, private_list)
+            self.histograms = np.array([estimated_histogram])
         else:
-            # print(f'time: {time}')
             estimated_histogram = self.perform_other_time_estimation(nr, time, private_list)
-            return estimated_histogram
+            self.histograms = np.vstack((self.histograms, estimated_histogram))
+        
+        return estimated_histogram
         
 
     
-    def perform_first_time_estimation(self, nr, time):
+    def perform_first_time_estimation(self, nr, time, private_list=None):
         # stores user submitted information for all times
-        self.noisy_reports = np.array(nr, dtype=int)
-        
-        hist = self.estimate_first_time(nr)
+        self.noisy_reports = np.array([nr], dtype=int)
+        self.prior = self.estimate_first_time(nr)
+        # self.prior = np.unique(private_list, return_counts=True)[-1] / len(nr)
         # stores estimated histogram for all times
-        self.histograms = np.array([hist])
+        # self.histograms = np.array([hist])
+        print(f'First time estimated: {self.prior}')
+        # calculate p_b1_bstar
+        eps_perm, p1, q1, eps_1, p2, q2 = self.get_instantaneous_randomization_configs(time)
+        p_b1_bstar = np.array([[p2, q2],
+                               [q2, p2]])
+        self.p_b_bstar = np.array([p_b1_bstar])
 
-        #calculating P(B^1|B=0) and P(B^1|B=1) for all users
-        probability_at_t = []
-        for i in range(len(nr)):
-            latestnr = nr[i] #latestnr corresponds to latest submitted value by the user
-            eps_perm, p1, q1, eps_1, p2, q2 = self.get_instantaneous_randomization_configs(time)
-            both_prob = []
-            for j in range(self.k):
-                if latestnr == j:
-                    both_prob.append(p1 * p2 + q1 * q2)
-                else:
-                    both_prob.append(p1 * q2 + p2 * q1)
-            probability_at_t.append(both_prob)
-
-        # stores P(B^T|B=0) and P(B^T|B=1) for all users at all times
-        self.noisy_reports_probability = np.array([probability_at_t])
-        return hist
+        #calculating P(B=0|B_1)
+        p_b_b1 = []
+        for user_index in range(len(nr)):
+            user_disclosed = self.noisy_reports[:,user_index]
+            numerator = self.prior[0]*self.p_bstar_b[0][0]* np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,0])  + self.prior[0]*self.p_bstar_b[1][0] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,1])
+            d_b_0 = numerator
+            d_b_1 = self.prior[1] * self.p_bstar_b[0][1] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,0]) + self.prior[1] * self.p_bstar_b[1][1] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,1])
+            denominator = d_b_0 + d_b_1
+            p_b_b1.append(numerator/denominator)
+        
+        estimated = np.array([np.sum(p_b_b1), len(nr)-np.sum(p_b_b1)])
+        estimated /= len(nr)
+        return estimated
+    
+    def estimate_first_time(self, nr):
+        eps_perm = self.epsilons[0][0]
+        eps_1 = self.epsilons[0][1]
+        return L_GRR_Aggregator_MI(nr, self.k, eps_perm, eps_1)
 
     def perform_other_time_estimation(self, nr, time, private_list=None):
             
         eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj = self.get_instantaneous_randomization_configs(time)
         self.noisy_reports = np.vstack((self.noisy_reports, nr))
-
+        p_b_bstar = np.ones((2,2))
+        prior = self.prior#self.histograms[-1]
         
+        all_b = [0, 1]
+        all_b_star = [0, 1]
 
-        # latest_hist = self.histograms[-1]
+        for b in all_b:
+            for b_star in all_b_star:
+                # if b == 0 and b_star == 0:
+                #     pp = p_maj * p1 * prior[0] + p_min * q1 * prior[1]
 
-        # majority = latest_hist.argmax()
-        # minority = latest_hist.argmin()
-        
-        # #for majority users : latest exp
-        # n_maj = int(latest_hist[majority] * len(nr))
-        # count_report = np.zeros(self.k)
-        # count_report[majority] = int((p1*p_maj + q1 * q_maj) * n_maj)
-        # count_report[minority] = int((p1*q_maj + q1 * p_maj) * n_maj)
-        # majority_freq = MI_long(count_report, n_maj, p1, q1, p_maj, q_maj)
-        # #for minority users
-        # n_min = int(latest_hist[minority] * len(nr))
-        # count_report = np.zeros(self.k)
-        # count_report[majority] = int((p1*p_min + q1 * q_min) * n_min)
-        # count_report[minority] = int((p1*q_min + q1 * p_min) * n_min)
-        # minority_freq = MI_long(count_report, n_min, p1, q1, p_min, q_min)
+                # elif b == 1 and b_star == 0:
+                #     pp = q_maj * p1 * prior[0] + q_min * q1 * prior[1]
 
-        
-        # weighted_frequency = (majority_freq * n_maj  + minority_freq * n_min) / (n_maj + n_min)
-        # self.histograms = np.vstack((self.histograms, weighted_frequency))
-        # return self.histograms[-1]
-        # majority_noisy_reports,minority_noisy_reports  = self.classify_users(eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj)
-        
+                # elif b == 0 and b_star == 1:
+                #     pp = q_maj * q1 * prior[0] + q_min * p1 * prior[1]
 
-        freq = self.classify_users_and_provide_freq(eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj, private_list)
-        # # h = np.unique(arrs, return_counts=True)[-1] / len(self.noisy_reports)
-        self.histograms = np.vstack((self.histograms, freq))
-        return self.histograms[-1]
-        
-        '''with perfect knowledge'''
-        # nr = np.array(nr)
-        # majority_noisy_reports = nr[private_list==self.histograms[-1].argmax()]
-        # minority_noisy_reports = nr[private_list==self.histograms[-1].argmin()]
-        
-        # majority_freq = L_GRR_Aggregator_IBU(majority_noisy_reports, self.k, eps_perm, eps_maj)
-        # minority_freq = np.array([0, 0])
-        
-        # if len(minority_noisy_reports) > 0:
-        # minority_freq = L_GRR_Aggregator_IBU(minority_noisy_reports, self.k, eps_perm, eps_min)
-        # print(f'Buckets: {len(majority_noisy_reports)}:{len(minority_noisy_reports)}')
-        # weighted_frequency = (majority_freq * len(majority_noisy_reports) + minority_freq * len(minority_noisy_reports)) / (len(majority_noisy_reports) + len(minority_noisy_reports))
-        # self.histograms = np.vstack((self.histograms, weighted_frequency))
-        # return self.histograms[-1]
+                # elif b == 1 and b_star == 1:
+                #     pp = p_maj * q1 * prior[0] + p_min * p1 * prior[1]
+                # if b == 0 and b_star == 0:
+                #     pp = p_maj * prior[0] + p_min * prior[1]
 
-   
-    def classify_users_and_provide_freq(self, eps_perm, p1, q1, eps_min, p_min, q_min, eps_maj, p_maj, q_maj, private_list=None):
-        """
-        Goal is to classify whether user belongs to majority or minority based on the submitted value at all times
-        """
+                # elif b == 1 and b_star == 0:
+                #     pp = q_maj *  prior[0] + q_min *  prior[1]
 
-        latest_hist = self.histograms[-1]
-        majority = latest_hist.argmax()
-        current_hist = [1] * 2
+                # elif b == 0 and b_star == 1:
+                #     pp = q_maj * prior[0] + q_min * prior[1]
 
-        prob_of_users_in_majority = self.calc_user_prob_majority(p1, q1, p_min, q_min, p_maj, q_maj, private_list)
-        
-        binomial_array = np.random.binomial(1, prob_of_users_in_majority)
-        if majority == 0:
-            current_hist[0] = len(binomial_array[binomial_array==1])
-            current_hist[1] = len(binomial_array[binomial_array==0])
-        else:
-            current_hist[1] = len(binomial_array[binomial_array==1])
-            current_hist[0] = len(binomial_array[binomial_array==0])
+                # elif b == 1 and b_star == 1:
+                #     pp = p_maj * prior[0] + p_min * prior[1]
 
-
-        return np.array(current_hist)/len(binomial_array)
-        # return np.array([len(binomial_array[binomial_array==1]), len(binomial_array[binomial_array==0])])/len(self.noisy_reports[-1])
-
-        # majority_bin = latest_nr_report[binomial_array==1]
-        # minority_bin = latest_nr_report[binomial_array==0]
-
-        return majority_bin, minority_bin
-
-    
-    def calc_user_prob_majority(self,p1, q1, p_min, q_min, p_maj, q_maj, private_list=None):
-        """
-        Calculates the probability whether the user belongs to majority or minority using bayesian technique 
-
-        outputs: P(B=majority|B^1, B^2, ....)
-        """
-
-
-        latest_histogram = self.histograms[-1]
-        
-        latest_nr_report = self.noisy_reports[-1]
-
-        majority = latest_histogram.argmax()
-        minority = latest_histogram.argmin()
-        probability_at_t = []
-        
-        
-        #for each user report calculate P(B^t|B=0) and P(B^t|B=1)
-        for n in range(len(latest_nr_report)):
-            both_prob = []
-            latestnr = latest_nr_report[n]
-
-            #for majority and minority separately, calculates P(B^t|B) for t = 1...
-            for b in range(self.k):
-
-                if b == majority:
-                    if latestnr == b:
-                        probb = p1 * p_maj + q1 * q_maj
+                if b_star == 0:
+                    prob1 = (prior[0] * p1)/(prior[0] * p1 + prior[1]*q1)
+                    prob2 = (prior[1] * q1)/(prior[1] * q1 + prior[0]*p1)
+                    if b == 0:
+                        pp = prob1 * p_maj + prob2 * p_min
                     else:
-                        probb = p1 * q_maj + q1 * p_maj
+                        pp = prob1 * q_maj + prob2 * q_min
+
                 else:
-                    if latestnr == b:
-                        probb = p1 * p_min + q1 * q_min
+                    prob1 = (prior[0] * q1)/(prior[0] * q1 + prior[1]*p1)
+                    prob2 = (prior[1] * p1)/(prior[0] * q1 + prior[1]*p1)
+                    if b == 0:
+                        pp = prob1 * q_maj + prob2 * q_min
                     else:
-                        probb = p1 * q_min + q1 * p_min
-                both_prob.append(probb)
-            probability_at_t.append(both_prob)
-        
+                        pp = prob1 * p_maj + prob2 * p_min
 
-        self.noisy_reports_probability = np.vstack((self.noisy_reports_probability, np.array([probability_at_t])))
-        prob_of_users_in_majority = []
-
-        """
-        For each user submitted value, calculate P(B=majority| B^1, B^2.....) using Bayesian technique
-        """
-        for n in range(len(latest_nr_report)):
-            latestnr = latest_nr_report[n]
-            user_n_probs = self.noisy_reports_probability[:,n]
-
-            # numerator = P(B=majority) * P(B^1|B=majority) * P(B^2|B=majority)
-
-            numerator = latest_histogram[majority] * np.prod(user_n_probs[:,majority])
-
-            denominator = 0
-
-            # denominator = sum over B = (majority and minority), P(P(B) * P(B^1|B) * P(B^2|B))
-            for b in range(self.k):
-                denominator += latest_histogram[b] * np.prod(user_n_probs[:,b])
-
-            p_user_majority = numerator/denominator
-            prob_of_users_in_majority.append(p_user_majority)
-        return np.array(prob_of_users_in_majority)
+                p_b_bstar[b][b_star] = pp
+        self.p_b_bstar = np.vstack((self.p_b_bstar, [p_b_bstar]))
 
 
+        posterior = []
+        for user_index in range(len(nr)):
+            user_disclosed = self.noisy_reports[:,user_index]
+            numerator = prior[0]*self.p_bstar_b[0][0]* np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,0])  + prior[0]*self.p_bstar_b[1][0] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,1])
+            d_b_0 = numerator
+            d_b_1 = prior[1] * self.p_bstar_b[0][1] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,0]) + prior[1] * self.p_bstar_b[1][1] * np.prod(self.p_b_bstar[np.arange(len(self.p_b_bstar)),user_disclosed][:,1])
+            denominator = d_b_0 + d_b_1
+            posterior.append(numerator/denominator)
 
 
+        estimated = np.array([np.sum(posterior), len(nr)-np.sum(posterior)])
+        estimated /= len(nr)
+        return estimated
 
 
         
+
+        
+
+        
+   
         
 
 
